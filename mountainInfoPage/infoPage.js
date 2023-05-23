d9f2c2914f600b5e3c306919ded0148684aa8506
davempark09 <43024345+davempark09@users.noreply.github.com>
2023-05-23 16:34:18 -0400

    Added functions for both pages

--- mountainInfoPage/infoPage.js
+++ mountainInfoPage/infoPage.js
@@ -1,7 +1,9 @@
 const mountainInfo = document.querySelector('#mountainInfo');
 const mountainParentCont = document.querySelector('#mountainInfoCards')
+const resetButton = document.querySelector('#resetSearch')
 
 window.onload = loadMountains
+resetButton.onclick = resetSearchResults
 
 function loadMountains() {
     mountainInfo.onchange = mountainInfoChange
@@ -13,6 +15,46 @@ function loadMountains() {
     }
 }
 
+function resetSearchResults() {
+    mountainParentCont.replaceChildren()
+}
+
 function mountainInfoChange(event) {
-    
+    const selectedMountain = event.target.value
+    const matchedMountain = mountainsArray.find(mount => mount.name === selectedMountain)
+    let mountainWikiLink;
+    if(matchedMountain.name.includes('Mt.')) {
+        mountainWikiLink = matchedMountain.name.replace('Mt.','Mount').split(' ').join('_')
+    } else {
+        mountainWikiLink = matchedMountain.name.split(' ').join('_')
+    }
+    mountainParentCont.replaceChildren()
+
+        const mountainCard = document.createElement('div')
+        mountainCard.setAttribute('class','card')
+        const mountainCardImg = document.createElement('img')
+        mountainCardImg.setAttribute('class','card-img-top')
+        mountainCardImg.setAttribute('src',`/images/${matchedMountain.img}`)
+        mountainCardImg.setAttribute('alt',matchedMountain.name)
+        const mountainCardBody = document.createElement('div')
+        mountainCardBody.setAttribute('class','card-body')
+        const mountrainCardTitle = document.createElement('h5')
+        mountrainCardTitle.setAttribute('class','card-title')
+        mountrainCardTitle.innerText = matchedMountain.name
+        const mountainCardDesc = document.createElement('p')
+        mountainCardDesc.setAttribute('class','card-text')
+        mountainCardDesc.innerText = matchedMountain.desc
+        const mountainCardElev = document.createElement('p')
+        mountainCardElev.setAttribute('class','card-text')
+        mountainCardElev.innerText = `Elevation: ${matchedMountain.elevation} feet`
+        const mountainMoreInfoButton = document.createElement('a')
+        mountainMoreInfoButton.setAttribute('class','btn btn-info')
+        mountainMoreInfoButton.setAttribute('href',`https://en.wikipedia.org/wiki/${mountainWikiLink}`)
+        mountainMoreInfoButton.setAttribute('role','button')
+        mountainMoreInfoButton.setAttribute('target','_blank')
+        mountainMoreInfoButton.innerText = 'More Information'
+
+        mountainCardBody.append(mountrainCardTitle,mountainCardDesc,mountainCardElev,mountainMoreInfoButton)
+        mountainCard.append(mountainCardImg,mountainCardBody)
+        mountainParentCont.append(mountainCard)
 }
--- nationalParkSearchPage/searchPage.js
+++ nationalParkSearchPage/searchPage.js
@@ -2,12 +2,17 @@ const locationSearch = document.querySelector('#searchLocation')
 const parkTypeSearch = document.querySelector('#searchParkType')
 const parkParentCont = document.querySelector('#parkSearchCards')
 const viewAllButton = document.querySelector('#viewAllButton')
+const resetButton = document.querySelector('#resetSearch')
 
 window.onload = loadSearch
-
+resetButton.onclick = resetSearchResults
 function loadSearch() {
     fillLocations()
     fillParkType()
+}
+
+function resetSearchResults() {
+    parkParentCont.replaceChildren()
 }
 
 function fillLocations() {
@@ -34,45 +39,89 @@ function changeLocations(event) {
     const selectedLocationSearch = event.target.value
     const selectedLocation = nationalParksArray.filter(location => location.State.toLowerCase() === selectedLocationSearch.toLowerCase())
     parkParentCont.replaceChildren()
-    for(const location of selectedLocation) {
-        const locationWikiLink = location.LocationName.split(' ').join('_')
-        const locationCard = document.createElement('div')
-        locationCard.setAttribute('class', 'card')
-        const locationCardBody = document.createElement('div')
-        locationCardBody.setAttribute('class', 'card-body')
-        const locationCardTitle = document.createElement('h5')
-        locationCardTitle.innerText = location.LocationName
-        const locationCardAddress = document.createElement('p')
-        locationCardAddress.innerText = `Address: ${location.Address}, ${location.City}, ${location.State} ${location.ZipCode}`
-        const locationCardPhoneFax = document.createElement('p')
-        locationCardPhoneFax.innerText = `Phone: ${location.Phone} / Fax: ${location.Fax}`
-        const locationCardLongLat = document.createElement('p')
-        locationCardLongLat.innerText = `Longitude: ${location.Longitude} / Latitude: ${location.Latitude}`
-        const locationMoreInfoButton = document.createElement('a')
-        locationMoreInfoButton.setAttribute('href',`https://en.wikipedia.org/wiki/${locationWikiLink}`)
-        locationMoreInfoButton.setAttribute('class','btn btn-info')
-        locationMoreInfoButton.setAttribute('role','button')
-        locationMoreInfoButton.setAttribute('target','_blank')
-        locationMoreInfoButton.innerText = 'More Information'
+    if(parkTypeSearch.value === '') {
+        for(const location of selectedLocation) {
+            const locationWikiLink = location.LocationName.split(' ').join('_')
+            const locationCard = document.createElement('div')
+            locationCard.setAttribute('class', 'card')
+            const locationCardBody = document.createElement('div')
+            locationCardBody.setAttribute('class', 'card-body')
+            const locationCardTitle = document.createElement('h5')
+            locationCardTitle.setAttribute('class','card-title')
+            locationCardTitle.innerText = location.LocationName
+            const locationCardAddress = document.createElement('p')
+            locationCardAddress.setAttribute('class','card-text')
+            locationCardAddress.innerText = `Address: ${location.Address}, ${location.City}, ${location.State} ${location.ZipCode}`
+            const locationCardPhoneFax = document.createElement('p')
+            locationCardPhoneFax.setAttribute('class','card-text')
+            locationCardPhoneFax.innerText = `Phone: ${location.Phone} / Fax: ${location.Fax}`
+            const locationCardLongLat = document.createElement('p')
+            locationCardLongLat.setAttribute('class','card-text')
+            locationCardLongLat.innerText = `Longitude: ${location.Longitude} / Latitude: ${location.Latitude}`
+            const locationMoreInfoButton = document.createElement('a')
+            locationMoreInfoButton.setAttribute('href',`https://en.wikipedia.org/wiki/${locationWikiLink}`)
+            locationMoreInfoButton.setAttribute('class','btn btn-info')
+            locationMoreInfoButton.setAttribute('role','button')
+            locationMoreInfoButton.setAttribute('target','_blank')
+            locationMoreInfoButton.innerText = 'More Information'
         
         
-        if(location.hasOwnProperty('Visit')) {
-            const locationWebsiteInfoButton = document.createElement('a')
-            locationWebsiteInfoButton.setAttribute('href',`${location.Visit}`)
-            locationWebsiteInfoButton.setAttribute('class','btn btn-info')
-            locationWebsiteInfoButton.setAttribute('role','button')
-            locationWebsiteInfoButton.setAttribute('target','_blank')
-            locationWebsiteInfoButton.innerText = 'Official Website'
-            locationCardBody.append(locationWebsiteInfoButton)
+            if(location.hasOwnProperty('Visit')) {
+                const locationWebsiteInfoButton = document.createElement('a')
+                locationWebsiteInfoButton.setAttribute('href',`${location.Visit}`)
+                locationWebsiteInfoButton.setAttribute('class','btn btn-info')
+                locationWebsiteInfoButton.setAttribute('role','button')
+                locationWebsiteInfoButton.setAttribute('target','_blank')
+                locationWebsiteInfoButton.innerText = 'Official Website'
+                locationCardBody.append(locationWebsiteInfoButton)
+            }
+        
+            locationCardBody.prepend(locationCardTitle,locationCardAddress,locationCardPhoneFax,locationCardLongLat,locationMoreInfoButton)
+            locationCard.append(locationCardBody)
+            parkParentCont.append(locationCard)
         }
+    } else {
+        const bothSearch = selectedLocation.filter(location => location.LocationName.toLowerCase().includes(parkTypeSearch.value.toLowerCase()))
+        for(const location of bothSearch) {
+            const locationWikiLink = location.LocationName.split(' ').join('_')
+            const locationCard = document.createElement('div')
+            locationCard.setAttribute('class', 'card')
+            const locationCardBody = document.createElement('div')
+            locationCardBody.setAttribute('class', 'card-body')
+            const locationCardTitle = document.createElement('h5')
+            locationCardTitle.setAttribute('class','card-title')
+            locationCardTitle.innerText = location.LocationName
+            const locationCardAddress = document.createElement('p')
+            locationCardAddress.setAttribute('class','card-text')
+            locationCardAddress.innerText = `Address: ${location.Address}, ${location.City}, ${location.State} ${location.ZipCode}`
+            const locationCardPhoneFax = document.createElement('p')
+            locationCardPhoneFax.setAttribute('class','card-text')
+            locationCardPhoneFax.innerText = `Phone: ${location.Phone} / Fax: ${location.Fax}`
+            const locationCardLongLat = document.createElement('p')
+            locationCardLongLat.setAttribute('class','card-text')
+            locationCardLongLat.innerText = `Longitude: ${location.Longitude} / Latitude: ${location.Latitude}`
+            const locationMoreInfoButton = document.createElement('a')
+            locationMoreInfoButton.setAttribute('href',`https://en.wikipedia.org/wiki/${locationWikiLink}`)
+            locationMoreInfoButton.setAttribute('class','btn btn-info')
+            locationMoreInfoButton.setAttribute('role','button')
+            locationMoreInfoButton.setAttribute('target','_blank')
+            locationMoreInfoButton.innerText = 'More Information'
         
-        locationCardBody.prepend(locationMoreInfoButton)
-        locationCardBody.prepend(locationCardLongLat)
-        locationCardBody.prepend(locationCardPhoneFax)
-        locationCardBody.prepend(locationCardAddress)
-        locationCardBody.prepend(locationCardTitle)
-        locationCard.append(locationCardBody)
-        parkParentCont.append(locationCard)
+        
+            if(location.hasOwnProperty('Visit')) {
+                const locationWebsiteInfoButton = document.createElement('a')
+                locationWebsiteInfoButton.setAttribute('href',`${location.Visit}`)
+                locationWebsiteInfoButton.setAttribute('class','btn btn-info')
+                locationWebsiteInfoButton.setAttribute('role','button')
+                locationWebsiteInfoButton.setAttribute('target','_blank')
+                locationWebsiteInfoButton.innerText = 'Official Website'
+                locationCardBody.append(locationWebsiteInfoButton)
+            }
+        
+            locationCardBody.prepend(locationCardTitle,locationCardAddress,locationCardPhoneFax,locationCardLongLat,locationMoreInfoButton)
+            locationCard.append(locationCardBody)
+            parkParentCont.append(locationCard)
+        }
     }
 }
 
@@ -80,6 +129,7 @@ function changeParkType(event) {
     const selectedParkTypeSearch = event.target.value
     const selectedParkType = nationalParksArray.filter(parkType => parkType.LocationName.toLowerCase().includes(selectedParkTypeSearch.toLowerCase()))
     parkParentCont.replaceChildren()
+    if(locationSearch.value === '') {
         for(const parkType of selectedParkType) {
         const parkTypeWikiLink = parkType.LocationName.split(' ').join('_')
         const parkTypeCard = document.createElement('div')
@@ -87,12 +137,16 @@ function changeParkType(event) {
         const parkTypeCardBody = document.createElement('div')
         parkTypeCardBody.setAttribute('class', 'card-body')
         const parkTypeCardTitle = document.createElement('h5')
+        parkTypeCardTitle.setAttribute('class','card-title')
         parkTypeCardTitle.innerText = parkType.LocationName
         const parkTypeCardAddress = document.createElement('p')
+        parkTypeCardAddress.setAttribute('class','card-text')
         parkTypeCardAddress.innerText = `Address: ${parkType.Address}, ${parkType.City}, ${parkType.State} ${parkType.ZipCode}`
         const parkTypeCardPhoneFax = document.createElement('p')
+        parkTypeCardPhoneFax.setAttribute('class','card-text')
         parkTypeCardPhoneFax.innerText = `Phone: ${parkType.Phone} / Fax: ${parkType.Fax}`
         const parkTypeCardLongLat = document.createElement('p')
+        parkTypeCardLongLat.setAttribute('class','card-text')
         parkTypeCardLongLat.innerText = `Longitude: ${parkType.Longitude} / Latitude: ${parkType.Latitude}`
         const parkTypeMoreInfoButton = document.createElement('a')
         parkTypeMoreInfoButton.setAttribute('href',`https://en.wikipedia.org/wiki/${parkTypeWikiLink}`)
@@ -112,19 +166,59 @@ function changeParkType(event) {
         }
         
 
-        parkTypeCardBody.prepend(parkTypeMoreInfoButton)
-        parkTypeCardBody.prepend(parkTypeCardLongLat)
-        parkTypeCardBody.prepend(parkTypeCardPhoneFax)
-        parkTypeCardBody.prepend(parkTypeCardAddress)
-        parkTypeCardBody.prepend(parkTypeCardTitle)
-        parkTypeCard.append(parkTypeCardBody)
-        parkParentCont.append(parkTypeCard)
+            parkTypeCardBody.prepend(parkTypeCardTitle,parkTypeCardAddress,parkTypeCardPhoneFax,parkTypeCardLongLat,parkTypeMoreInfoButton)
+            parkTypeCard.append(parkTypeCardBody)
+            parkParentCont.append(parkTypeCard)
+        }
+    } else {
+        const bothSearch = selectedParkType.filter(parkType => parkType.State.toLowerCase() === locationSearch.value.toLowerCase())
+        for(const parkType of bothSearch) {
+        const parkTypeWikiLink = parkType.LocationName.split(' ').join('_')
+        const parkTypeCard = document.createElement('div')
+        parkTypeCard.setAttribute('class', 'card')
+        const parkTypeCardBody = document.createElement('div')
+        parkTypeCardBody.setAttribute('class', 'card-body')
+        const parkTypeCardTitle = document.createElement('h5')
+        parkTypeCardTitle.setAttribute('class','card-title')
+        parkTypeCardTitle.innerText = parkType.LocationName
+        const parkTypeCardAddress = document.createElement('p')
+        parkTypeCardAddress.setAttribute('class','card-text')
+        parkTypeCardAddress.innerText = `Address: ${parkType.Address}, ${parkType.City}, ${parkType.State} ${parkType.ZipCode}`
+        const parkTypeCardPhoneFax = document.createElement('p')
+        parkTypeCardPhoneFax.setAttribute('class','card-text')
+        parkTypeCardPhoneFax.innerText = `Phone: ${parkType.Phone} / Fax: ${parkType.Fax}`
+        const parkTypeCardLongLat = document.createElement('p')
+        parkTypeCardLongLat.setAttribute('class','card-text')
+        parkTypeCardLongLat.innerText = `Longitude: ${parkType.Longitude} / Latitude: ${parkType.Latitude}`
+        const parkTypeMoreInfoButton = document.createElement('a')
+        parkTypeMoreInfoButton.setAttribute('href',`https://en.wikipedia.org/wiki/${parkTypeWikiLink}`)
+        parkTypeMoreInfoButton.setAttribute('class','btn btn-info')
+        parkTypeMoreInfoButton.setAttribute('role','button')
+        parkTypeMoreInfoButton.setAttribute('target','_blank')
+        parkTypeMoreInfoButton.innerText = 'More Information'
+
+        if(parkType.hasOwnProperty('Visit')) {
+            const parkTypeWebsiteInfoButton = document.createElement('a')
+            parkTypeWebsiteInfoButton.setAttribute('href',`${parkType.Visit}`)
+            parkTypeWebsiteInfoButton.setAttribute('class','btn btn-info')
+            parkTypeWebsiteInfoButton.setAttribute('role','button')
+            parkTypeWebsiteInfoButton.setAttribute('target','_blank')
+            parkTypeWebsiteInfoButton.innerText = 'Official Website'
+            parkTypeCardBody.append(parkTypeWebsiteInfoButton)
+        }
+        
+
+            parkTypeCardBody.prepend(parkTypeCardTitle,parkTypeCardAddress,parkTypeCardPhoneFax,parkTypeCardLongLat,parkTypeMoreInfoButton)
+            parkTypeCard.append(parkTypeCardBody)
+            parkParentCont.append(parkTypeCard)
+        }
     }
 }
 
 viewAllButton.onclick = viewAllEvent
 
-function viewAllEvent() {
+function viewAllEvent(event) {
+    event.preventDefault()
     parkParentCont.replaceChildren()
         for(const allParks of nationalParksArray) {
         const allParksWikiLink = allParks.LocationName.split(' ').join('_')
@@ -133,12 +227,16 @@ function viewAllEvent() {
         const allParksCardBody = document.createElement('div')
         allParksCardBody.setAttribute('class', 'card-body')
         const allParksCardTitle = document.createElement('h5')
+        allParksCardTitle.setAttribute('class','card-title')
         allParksCardTitle.innerText = allParks.LocationName
         const allParksCardAddress = document.createElement('p')
+        allParksCardAddress.setAttribute('class','card-text')
         allParksCardAddress.innerText = `Address: ${allParks.Address}, ${allParks.City}, ${allParks.State} ${allParks.ZipCode}`
         const allParksCardPhoneFax = document.createElement('p')
+        allParksCardPhoneFax.setAttribute('class','card-text')
         allParksCardPhoneFax.innerText = `Phone: ${allParks.Phone} / Fax: ${allParks.Fax}`
         const allParksCardLongLat = document.createElement('p')
+        allParksCardLongLat.setAttribute('card','card-text')
         allParksCardLongLat.innerText = `Longitude: ${allParks.Longitude} / Latitude: ${allParks.Latitude}`
         const allParksMoreInfoButton = document.createElement('a')
         allParksMoreInfoButton.setAttribute('href',`https://en.wikipedia.org/wiki/${allParksWikiLink}`)
@@ -157,11 +255,7 @@ function viewAllEvent() {
             allParksCardBody.append(allParksWebsiteInfoButton)
         }
 
-        allParksCardBody.prepend(allParksMoreInfoButton)
-        allParksCardBody.prepend(allParksCardLongLat)
-        allParksCardBody.prepend(allParksCardPhoneFax)
-        allParksCardBody.prepend(allParksCardAddress)
-        allParksCardBody.prepend(allParksCardTitle)
+        allParksCardBody.prepend(allParksCardTitle,allParksCardAddress,allParksCardPhoneFax,allParksCardLongLat,allParksMoreInfoButton)
         allParksCard.append(allParksCardBody)
         parkParentCont.append(allParksCard)
     }
